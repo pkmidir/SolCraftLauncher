ea1573bd41c5812a5ec8addd336487a5cc52c13a
SolDev69 <40839581+SolDev69@users.noreply.github.com>
2023-12-02 08:36:43 -0500

    let's try something
    Lower minimum OS requirement

--- app_pojavlauncher/src/main/jni/egl_bridge.c
+++ app_pojavlauncher/src/main/jni/egl_bridge.c
@@ -173,7 +173,7 @@ static void set_vulkan_ptr(void* ptr) {
 
 void load_vulkan() {
     if(getenv("POJAV_ZINK_PREFER_SYSTEM_DRIVER") == NULL &&
-        android_get_device_api_level() >= 28) { // the loader does not support below that
+        android_get_device_api_level() >= 27) { // the loader does not support below that
 #ifdef ADRENO_POSSIBLE
         void* result = load_turnip_vulkan();
         if(result != NULL) {
