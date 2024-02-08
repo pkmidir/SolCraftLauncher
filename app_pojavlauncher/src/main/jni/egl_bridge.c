70ae097c0d99ae43b82564b15020bc4484e18896
SolDev69 <40839581+SolDev69@users.noreply.github.com>
2024-02-08 18:34:37 -0500

    Update virgl run script
    Imagine this fixes new vtest

--- app_pojavlauncher/src/main/jni/egl_bridge.c
+++ app_pojavlauncher/src/main/jni/egl_bridge.c
@@ -443,7 +443,7 @@ void* egl_make_current(void* window) {
     if (pojav_environ->config_renderer == RENDERER_VIRGL) {
         printf("VirGL: vtest_main = %p\n", vtest_main_p);
         printf("VirGL: Calling VTest server's main function\n");
-        vtest_main_p(3, (const char*[]){"vtest", "--no-loop-or-fork", "--use-gles", NULL, NULL});
+        vtest_main_p(4, (const char*[]){"vtest", "--no-loop-or-fork", "--use-gles", "--use-egl-surfaceless", NULL, NULL});
     }
 }
 
