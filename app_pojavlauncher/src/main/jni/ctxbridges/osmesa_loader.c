6423023b73ba48db4498c17038f7d08dfdb14c21
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2024-03-19 19:06:39 +0800

    Update osmesa_loader.c

--- app_pojavlauncher/src/main/jni/ctxbridges/osmesa_loader.c
+++ app_pojavlauncher/src/main/jni/ctxbridges/osmesa_loader.c
@@ -1,5 +1,5 @@
 //
-// Modifile by Vera-Firefly on 13.01.2024.
+// Modifiled by Vera-Firefly on 19.03.2024.
 //
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,18 +21,14 @@ void (*glClear_p) (GLbitfield mask);
 void (*glReadPixels_p) (GLint x, GLint y, GLsizei width, GLsizei height, GLenum format, GLenum type, void * data);
 
 void dlsym_OSMesa() {
-    char* main_osm_libname = getenv("POJAVEXEC_OSMESA");
     char* main_path = NULL;
-    if(pojav_environ->config_renderer == RENDERER_VK_ZINK) {
-        if(main_osm_libname == NULL) main_osm_libname = "libOSMesa.so";
-        if(asprintf(&main_path, "%s/%s", getenv("POJAV_NATIVEDIR"), main_osm_libname) == -1) {
+    char* mesa_library = getenv("MESA_LIBRARY");
+    if(pojav_environ->config_renderer == RENDERER_VK_ZINK
+    || pojav_environ->config_renderer == RENDERER_VIRGL) {
+        if(asprintf(&main_path, "%s/%s", getenv("POJAV_NATIVEDIR"), mesa_library) == -1) {
             abort();
         }
         free(main_path);
-    } else if(pojav_environ->config_renderer == RENDERER_VIRGL) {
-        if(asprintf(&main_path, "%s/libOSMesa_znL.so", getenv("POJAV_NATIVEDIR")) == -1) {
-            abort();
-        }
     }
     void* dl_handle = NULL;
     dl_handle = dlopen(main_path, RTLD_GLOBAL);
